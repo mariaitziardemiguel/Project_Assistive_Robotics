9ce1518703be210a4624be85779b223fa722da85
loreamazquiaran <lmazquec37@alumnes.ub.edu>
2025-09-30 15:32:25 +0200

    Código_SW_HW python

--- src/python_scripts/Assistive_hand_SW_HW.py
+++ src/python_scripts/Assistive_hand_SW_HW.py
@@ -18,10 +18,28 @@ robot = RDK.Item("UR5e")
 base = RDK.Item("UR5e Base")
 tool = RDK.Item('Hand')
 Init_target = RDK.Item('Init')
-App_shake_target = RDK.Item('App_shake')
-Shake_target = RDK.Item('Shake')
-App_give5_target = RDK.Item('App_give5')
-Give5_target = RDK.Item('Give5')
+App_wave_target = RDK.Item('App_wave')
+Wave_target = RDK.Item('Wave')
+Bajada_target = RDK.Item('Bajada')
+Subida_target = RDK.Item('Subida')
+Bajada2_target = RDK.Item('Bajada2')
+Dab1_target = RDK.Item('Dab1')
+Dab2_target = RDK.Item('Dab2')
+Dab3_target = RDK.Item('Dab3')
+Dab4_target = RDK.Item('Dab4')
+Dab5_target = RDK.Item('Dab5')
+Stop_target = RDK.Item('Stop')
+Go1_target = RDK.Item('Go1')
+Go2_target = RDK.Item('Go2')
+Go3_target = RDK.Item('Go3')
+Emergency1_target = RDK.Item('Emergency1')
+Emergency2_target = RDK.Item('Emergency2')
+Emergency3_target = RDK.Item('Emergency3')
+RCP_up_target = RDK.Item('RCP_up')
+RCP_down_target = RDK.Item('RCP_down')
+Out_target = RDK.Item('Out')
+In_target = RDK.Item('In')
+
 
 # Set robot frame, tool and speed
 robot.setPoseFrame(base)
@@ -45,28 +63,83 @@ def robot_online(online):
         print("Simulation mode activated.")
 
 # Robot movements
-def move_to_init():
+def Init():
     print("Init")
+    robot.setSpeed(20)
     robot.MoveL(Init_target, True)
     print("Init_target REACHED")
+    
 
-def hand_shake():
-    print("Hand Shake")
-    robot.setSpeed(50)
-    robot.MoveL(App_shake_target, True)
-    robot.setSpeed(100)
-    robot.MoveL(Shake_target, True)
-    robot.MoveL(App_shake_target, True)
-    print("Hand Shake FINISHED")
+def Hand_wave():
+    print("Hand Wave")
+    robot.setSpeed(40)
+    robot.MoveL(App_wave_target, True)
+    robot.setSpeed(10)
+    robot.MoveL(Wave_target, True)
+    robot.setSpeed(30)
+    robot.MoveL(Bajada_target, True)
+    robot.MoveL(Subida_target, True)
+    robot.MoveL(Bajada2_target, True)
+    print("Hand Wave FINISHED")
+    
+def Dab():
+    print("Dab!")
+    robot.MoveL(Dab1_target, True)
+    robot.MoveL(Dab2_target, True)
+    robot.MoveL(Dab3_target, True)
+    robot.MoveL(Dab4_target, True)
+    robot.setSpeed(30)
+    robot.MoveL(Dab5_target, True)
+    print("Dab! FINISHED")
 
-def give_me_5():
-    print("Give me 5!")
+def Stop_and_go():
+    print("Stop and go")
+    robot.MoveL(Stop_target, True)
+    time.sleep(2)
+    robot.MoveL(Go1_target, True)
     robot.setSpeed(50)
-    robot.MoveL(App_give5_target, True)
+    robot.MoveL(Go2_target, True)
+    robot.MoveL(Go3_target, True)
+    robot.MoveL(Go2_target, True)
+    robot.MoveL(Go3_target, True)
+    print("Stop and go FINISHED")
+
+def Emergency():
+    print("Emergency")
+    robot.setSpeed(40)
+    robot.MoveL(Emergency1_target, True)
+    robot.setSpeed(90)
+    robot.MoveL(Emergency2_target, True)
+    robot.MoveL(Emergency3_target, True)
+    robot.MoveL(Emergency2_target, True)
+    robot.MoveL(Emergency1_target, True)
+    print("Emergency FINISHED")
+
+def RCP():
+    print("Starting RCP...")
+    robot.setSpeed(20)
+    robot.MoveL(RCP_up_target, True)
     robot.setSpeed(100)
-    robot.MoveL(Give5_target, True)
-    robot.MoveL(App_give5_target, True)
-    print("Give me 5! FINISHED")
+    cycles = 6
+    for i in range(cycles):
+        print(f"Compression {i+1}")
+        robot.MoveL(RCP_down_target, True)
+        robot.MoveL(RCP_up_target, True)
+    print("RCP FINISHED")
+
+#def Heimlich():
+    #print("Heimlich")
+    #robot.setSpeed(20)
+    #robot.MoveL(Out_target, True)
+    #robot.setSpeed(100)
+    #robot.MoveL(In_target, True)
+    #robot.setSpeed(20)
+    #robot.MoveL(Out_target, True)
+    #robot.setSpeed(100)
+    #robot.MoveL(In_target, True)
+    #robot.setSpeed(20)
+    #robot.MoveL(Out_target, True)
+    #print("Heimlich FINISHED")
 
 # Confirmation dialog to close RoboDK
 def confirm_close():
